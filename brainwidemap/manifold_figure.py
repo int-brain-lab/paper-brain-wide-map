6a121fd5cfe56b97852fd3d038b9920e43b63524
mschart <michael.m.schartner@gmail.com>
2022-06-07 13:25:03 +0100

    Update manifold_figure.py

--- brainwidemap/manifold_figure.py
+++ brainwidemap/manifold_figure.py
@@ -3,6 +3,8 @@ from reproducible_ephys_processing import bin_spikes2D
 from ibllib.atlas import AllenAtlas
 from ibllib.atlas.regions import BrainRegions
 from brainbox.io.one import SpikeSortingLoader
+from brainwidemap import bwm_query
+
 
 from ibllib.atlas.plots import prepare_lr_data
 from ibllib.atlas.plots import plot_scalar_on_flatmap, plot_scalar_on_slice
@@ -208,77 +210,6 @@ def get_psths_atlasids(split,eid, probe, control=False):
 ### bulk processing 
 ###    
 '''  
-
-
-def bwm_query(one, alignment_resolved=True, return_details=False):
-    """
-    Function to query for brainwide map sessions that pass the most important quality controls. Returns a dataframe
-    with one row per insertions and columns ['pid', 'eid', 'probe_name', 'session_number', 'date', 'subject', 'lab']
-    Parameters
-    ----------
-    one: ONE instance (can be remote or local)
-    alignment_resolved: bool, default is True. If True, only returns sessions with resolved alignment,
-                        if False returns all sessions with at least one alignment
-    return_details: bool, default is False. If True returns a second output a list containing the full insertion
-                    dictionary for all insertions returned by the query. Only needed if you need information that is
-                    not contained in the output dataframe
-    Returns
-    -------
-    bwm_df: pd.DataFrame of BWM sessions to be included in analyses with columns
-            ['pid', 'eid', 'probe_name', 'session_number', 'date', 'subject', 'lab']
-    """
-
-    base_query = (
-        'session__project__name__icontains,ibl_neuropixel_brainwide_01,'
-        'session__json__IS_MOCK,False,'
-        'session__qc__lt,50,'
-        '~json__qc,CRITICAL,'
-        'session__extended_qc__behavior,1,'
-        'json__extended_qc__tracing_exists,True,'
-    )
-
-    if alignment_resolved:
-        base_query += 'json__extended_qc__alignment_resolved,True,'
-    else:
-        base_query += 'json__extended_qc__alignment_count__gt,0,'
-
-    qc_pass = (
-        '~session__extended_qc___task_stimOn_goCue_delays__lt,0.9,'
-        '~session__extended_qc___task_response_feedback_delays__lt,0.9,'
-        '~session__extended_qc___task_wheel_move_before_feedback__lt,0.9,'
-        '~session__extended_qc___task_wheel_freeze_during_quiescence__lt,0.9,'
-        '~session__extended_qc___task_error_trial_event_sequence__lt,0.9,'
-        '~session__extended_qc___task_correct_trial_event_sequence__lt,0.9,'
-        '~session__extended_qc___task_reward_volumes__lt,0.9,'
-        '~session__extended_qc___task_reward_volume_set__lt,0.9,'
-        '~session__extended_qc___task_stimulus_move_before_goCue__lt,0.9,'
-        '~session__extended_qc___task_audio_pre_trial__lt,0.9')
-
-    marked_pass = (
-        'session__extended_qc___experimenter_task,PASS')
-
-    insertions = list(one.alyx.rest('insertions', 'list', django=base_query + qc_pass))
-    insertions.extend(list(one.alyx.rest('insertions', 'list', django=base_query + marked_pass)))
-
-    bwm_df = pd.DataFrame({
-        'pid': np.array([i['id'] for i in insertions]),
-        'eid': np.array([i['session'] for i in insertions]),
-        'probe_name': np.array([i['name'] for i in insertions]),
-        'session_number': np.array([i['session_info']['number'] 
-                                    for i in insertions]),
-        'date': np.array([parser.parse(i['session_info']['start_time']).date() 
-                          for i in insertions]),
-        'subject': np.array([i['session_info']['subject'] for i in insertions]),
-        'lab': np.array([i['session_info']['lab'] for i in insertions]),
-    }).sort_values(by=['lab', 'subject', 'date', 'eid'])
-    bwm_df.drop_duplicates(inplace=True)
-    bwm_df.reset_index(inplace=True, drop=True)
-
-    if return_details:
-        return bwm_df, insertions
-    else:
-        return bwm_df
-   
 
 def download_data_only():
     df = bwm_query(one)
