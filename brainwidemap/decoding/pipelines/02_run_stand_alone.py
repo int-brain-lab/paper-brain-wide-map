2965bf809aad7d95a1c5fda9b777ba63538248f1
juhuntenburg <ju.huntenburg@gmail.com>
2022-10-06 18:08:05 +0100

    loading wheel and whisker

--- brainwidemap/decoding/pipelines/02_run_stand_alone.py
+++ brainwidemap/decoding/pipelines/02_run_stand_alone.py
@@ -14,45 +14,20 @@ import sys
 import yaml
 
 from one.api import ONE
+from brainbox.io.one import SessionLoader
+from brainwidemap import bwm_query, load_good_units, load_trials_and_mask
 
 from braindelphi.utils_root import load_pickle_data
-from braindelphi.params import CACHE_PATH, SETTINGS_PATH, FIT_PATH, IMPOSTER_SESSION_PATH
-from braindelphi.pipelines.utils_common_pipelines import load_ephys, load_behavior
+from braindelphi.params import FIT_PATH, IMPOSTER_SESSION_PATH
 from braindelphi.decoding.functions.decoding import fit_eid
 from braindelphi.decoding.functions.utils import check_settings
 
-
-def load_metadata(neural_dtype_path_regex, date=None):
-    '''
-    Parameters
-    ----------
-    neural_dtype_path_regex
-    date
-    Returns
-    -------
-    the metadata neural_dtype from date if specified, else most recent
-    '''
-    neural_dtype_paths = glob.glob(neural_dtype_path_regex)
-    neural_dtype_dates = [datetime.strptime(p.split('/')[-1].split('_')[0], '%Y-%m-%d')
-                          for p in neural_dtype_paths]
-    if date is None:
-        path_id = np.argmax(neural_dtype_dates)
-    else:
-        path_id = np.argmax(np.array(neural_dtype_dates) == date)
-    return pickle.load(open(neural_dtype_paths[path_id], 'rb')), neural_dtype_dates[path_id].strftime("%m-%d-%Y_%H:%M:%S")
-
-
-# sessions to be excluded (by Olivier Winter)
-excludes = [
-    'bb6a5aae-2431-401d-8f6a-9fdd6de655a9',  # inconsistent trials object: relaunched on 31-12-2021
-    'c7b0e1a3-4d4d-4a76-9339-e73d0ed5425b',  # same same
-    '7a887357-850a-4378-bd2a-b5bc8bdd3aac',  # same same
-    '56b57c38-2699-4091-90a8-aba35103155e',  # load obect pickle error
-    '09394481-8dd2-4d5c-9327-f2753ede92d7',  # same same
-]
-
+# Determines below and above which sessions should not be decoded
+IMIN = 10
+IMAX = 1000
 
 # load settings as a dict
+SETTINGS_PATH = Path('/home/julia/workspace/int-brain-lab/paper-brain-wide-map/brainwidemap/decoding/settings_default.yaml')
 settings = yaml.safe_load(open(SETTINGS_PATH))
 kwargs = check_settings(settings)
 # add path info
@@ -70,11 +45,11 @@ logging.basicConfig(
 )
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))  # add logging to console
 
-# load insertion data
-bwm_dict, _ = load_metadata(
-    CACHE_PATH.joinpath('*_%s_metadata.pkl' % kwargs['neural_dtype']).as_posix())
-bwm_df = bwm_dict['dataset_filenames']
-eids = bwm_df['eid'].unique()
+# Instantiate ONE
+one = ONE()
+
+# Get the set of sessions
+bwm_df = bwm_query()
 
 # load imposter session df
 if kwargs.get('n_pseudo', 0) > 0:
@@ -86,63 +61,76 @@ else:
     kwargs['imposter_df'] = None
 
 # loop over sessions: load data and decode
-one = ONE()
-IMIN = 10
-IMAX = 1000
-for i, eid in enumerate(eids):
 
+for i, eid in enumerate(bwm_df['eid'].unique()):
     # determine if we should proceed with decoding session
     if i < IMIN:
         continue
     if i >= IMAX:
         continue
-    if eid in excludes:
+
+    # construct metadata dict for now
+    metadata = {
+        'subject': bwm_df.iloc[i]['subject'],
+        'eid': bwm_df.iloc[i]['eid'],
+        'probe_name': bwm_df.iloc[i]['probe_name']
+    }
+    logging.log(logging.DEBUG, f"{i}, session: {eid}, subject: {metadata['subject']}")
+
+    # load trials data (same for all probes)
+    # add start and end times (for now)
+    try:
+        sess_loader = SessionLoader(one, metadata['eid'])
+        sess_loader.load_trials()
+        sess_loader.trials['trial_start'] = sess_loader.trials['stimOn_times'] - 0.6
+        sess_loader.trials['trial_end'] = sess_loader.trials['stimOn_times'] + 0.6
+    except Exception as e:
+        logging.log(logging.CRITICAL, e)
         continue
 
-    # use pre-computed pkl files
-    curr_df = bwm_df[bwm_df['eid'] == eid]
-    subject = curr_df.subject.iloc[0]
+    # Load whichever target data is required and put it into "dlc_dict" for now
+    try:
+        if kwargs['target'] == 'wheel-vel':
+            sess_loader.load_wheel()
+            dlc_dict = {'times': sess_loader.wheel['times'], 'values': sess_loader.wheel['velocity']}
+        elif kwargs['target'] == 'wheel-speed':
+            sess_loader.load_wheel()
+            dlc_dict = {'times': sess_loader.wheel['times'], 'values': np.abs(sess_loader.wheel['velocity'])}
+        elif kwargs['target'] == 'l-whisker-me':
+            sess_loader.load_motion_energy(views=['left'])
+            dlc_dict = {'times': sess_loader.motion_energy['leftCamera']['times'],
+                        'values': sess_loader.motion_energy['leftCamera']['whiskerMotionEnergy']}
+        elif kwargs['target'] == 'r-whisker-me':
+            sess_loader.load_motion_energy(views=['right'])
+            dlc_dict = {'times': sess_loader.motion_energy['rightCamera']['times'],
+                        'values': sess_loader.motion_energy['rightCamera']['whiskerMotionEnergy']}
+        dlc_dict['skip': False]
+    except BaseException as e:
+        logging.info(e)
+        dlc_dict = {'times': None, 'values': None, 'skip': True}
 
-    # load behavioral data (same for all probes)
-    filename = '%s_%s_regressors.pkl' % (kwargs['date'], kwargs['target'])
-    beh_cache = Path(CACHE_PATH).joinpath('behavior', subject, eid, filename)
-    # need to check if this exists since we're iterating through the neural data cache
-    if beh_cache.exists():
-        dlc_dict = load_pickle_data(beh_cache)
-    else:
-        logging.log(
-            logging.DEBUG,
-            f"{i}, session: {eid}, subject: {subject} - NO {kwargs['target']} DATA")
-        continue
-
-    if dlc_dict['skip']:
-        continue
-
-    logging.log(logging.DEBUG, f"{i}, session: {eid}, subject: {subject}")
-
-    for pid_id in range(curr_df.index.size):
-
-        pid = curr_df.iloc[pid_id]
-
+    # For each probe of this session, load neural data and run fit_eid
+    for pid, pname in bwm_df[bwm_df['eid']==eid].filter(['pid', 'probe_name']).itertuples(index=False):
         # load neural data
         try:
-            metadata = pickle.load(open(pid.meta_file, 'rb'))
-            regressors = pickle.load(open(pid.reg_file, 'rb'))
-            if kwargs['neural_dtype'] == 'widefield':
-                trials_df, neural_dict = regressors
-            else:
-                trials_df, neural_dict = regressors['trials_df'], regressors
+            spikes, clusters = load_good_units(one, pid, eid=eid, pname=pname)
+            neural_dict = {
+                'spk_times': spikes['times'],
+                'spk_clu': spikes['clusters'],
+                'clu_regions': clusters['acronym'],
+                'clu_qc': {k: np.asarray(v) for k, v in clusters.to_dict('list').items()},
+                'clu_df': clusters
+            }
         except Exception as e:
             logging.log(logging.CRITICAL, e)
             continue
 
         filenames = fit_eid(
             neural_dict=neural_dict,
-            trials_df=trials_df,
+            trials_df=sess_loader.trials,
             metadata=metadata,
             dlc_dict=dlc_dict,
             pseudo_ids=[-1],
-            # pseudo_ids=1 + np.arange(kwargs['n_pseudo']),
             **copy.copy(kwargs)
         )
         print(filenames)
