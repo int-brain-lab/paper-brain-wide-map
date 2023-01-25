8d00c6b0849b8f62da8bcead5904ad4fdeef5af7
mschart <michael.m.schartner@gmail.com>
2023-01-25 14:45:58 +0000

    clean up plot_all, transpose panels

--- brainwidemap/manifold/state_space_bwm.py
+++ brainwidemap/manifold/state_space_bwm.py
@@ -44,9 +44,7 @@ np.set_printoptions(threshold=sys.maxsize)
 plt.rcParams.update(plt.rcParamsDefault)
 plt.ion()
 
-
 f_size = 15
-
 
 def set_sizes(f_s=f_size):
     plt.rc('font', size=f_s)
@@ -58,7 +56,6 @@ def set_sizes(f_s=f_size):
     plt.rc('figure', titlesize=f_s)
 
 # set_sizes()
-
 # plt.rcParams['figure.constrained_layout.use'] = True
 
 
@@ -80,13 +77,12 @@ nlicks = 50  # trial window length in licks
 # trial split types, see get_d_vars for details
 align = {'stim': 'stim on',
          'choice': 'motion on',
-         'fback': 'feedback'},
-         #'block': 'stim on'}
-         #'choice_restr': 'motion_on'}#
-         #'stim': 'stim on'
-         
-         #,
-         #'block': 'stim on'}
+         'fback': 'feedback',
+         'block': 'stim on',
+         'stim_restr': 'stim on',
+         'choice_restr': 'motion on',
+         'fback_restr': 'feedback',
+         'block_restr': 'stim on'}
 
 # contrasts: 0.    , 0.0625, 0.125 , 0.25  , 1.    ,    nan
 
@@ -897,30 +893,29 @@ def put_panel_label(ax, k):
                 ha='right', weight='bold')
 
 
-def plot_all(curve='euc', amp_number=False, intro=True,
-             sigl=0.01, only3d=False, onlyScat=False,
-             single_scat=False, auto_label_adjust=False,
-             ga_pcs=True):
+def plot_all(curve='euc', amp_number=False,
+             sigl=0.01, ga_pcs=True, splits = None):
+             
     '''
     main figure: show example trajectories,
     d_var_m for select regions and scatter all sigificant regions
     curve in [var, euc]
     sigl: significance level, default 0.05
-    intro: if False, don't plot schematics of method and contrast
-    only3d: only 3d plots
+
     '''
+    if splits is None:
+        splits = align
+    
 
-
-    nrows = 12
-
-    fig = plt.figure(figsize=(10, 10 * (2**.5)))  # 10, 15
-    gs = fig.add_gridspec(nrows, len(align))
+    # specify grid; scatter longer than other panels
+    ncols = 12  
+    fig = plt.figure(figsize=(15, 15))  
+    gs = fig.add_gridspec(len(splits), ncols)
+    axs = []
+    k = 0  # panel counter
 
     dfa, palette = get_allen_info()
 
-    axs = []
-    k = 0  # panel counter
-    row = 0  # row counter
 
     '''
     get significant regions
@@ -928,7 +923,7 @@ def plot_all(curve='euc', amp_number=False, intro=True,
     tops = {}
     regsa = []
 
-    for split in align:
+    for split in splits:
 
         d = np.load(Path(pth_res, f'{split}.npy'),
                     allow_pickle=True).flat[0]
@@ -966,234 +961,172 @@ def plot_all(curve='euc', amp_number=False, intro=True,
 
     cosregs = dict(zip(regsa, cosregs_))
 
-    v = 0 #if intro else 3  # indexing rows
 
     '''
     example regions per split for embedded space and line plots
     '''
 
-#    tops_p = {split: [tops[split][0][j] for j in range(len(tops[split][0]))
-#                      if tops[split][1][j] < sigl] for split in align}
-
-#    exs = {split: list(set(exs0[split]
-#                  ).intersection(set(tops_p[split])))
-#                  for split in align}
-
-    exs = {'stim': ['PRNr','LGd', 'LP', 'VISpm', 'VISp', 
+    exs0 = {'stim': ['PRNr','LGd', 'LP', 'VISpm', 'VISp', 
                     'VISam', 'MRN', 'SCm', 'IP', 'IRN'],
                     
            'choice': ['ACAv', 'PRNr', 'LP', 'SIM', 'APN',
-                      'MRN', 'RT', 'IRN', 'IP', 'GRN'],
-                      
-           'choice_restr': ['ACAv', 'PRNr', 'LP', 'SIM', 'APN',
-                      'MRN', 'RT', 'IRN', 'IP', 'GRN'],                      
+                      'MRN', 'RT', 'IRN', 'IP', 'GRN'],                  
 
            'fback': ['IRN', 'SSp-m', 'PRNr', 'IC', 'MV', 'AUDp',
                      'CENT3', 'SSp-ul', 'GPe'],
            'block': ['Eth', 'IC']}
+           
+    exs = exs0.copy()        
+    for split in exs0:      
+        exs[f'{split}_restr'] = exs[split]        
+           
+           
+       
 
-    # old choice: ['GRN','ACAv', 'APN', 'IRN', 'PRM',
-    # 'PRNr', 'RT', 'SIM', 'SSp-ul', 'ZI'],
+    '''
+    Trajectories for example regions in PCA embedded 3d space
+    '''
 
-    if not onlyScat:
+    
+    row = 0
+    for split in splits:
 
-        '''
-        load schematic intro and contrast trajectory plot (convert from svg)
-        '''
+        if ga_pcs:
+            d = np.load(Path(pth_res, f'{split}_grand_averages.npy'),
+                        allow_pickle=True).flat[0]
+        else:
+            d = np.load(Path(pth_res, f'{split}.npy'),
+                        allow_pickle=True).flat[0][split]
 
-        if intro:
+            # pick example region
+            reg = exs[split][0]
 
-            axs.append(fig.add_subplot(gs[:3, :]))
+        axs.append(fig.add_subplot(gs[row, :3],
+                                   projection='3d'))
 
-            pdf = fitz.open(Path(pth_res, 'intro2.pdf'))
-            rgb = pdf[0].get_pixmap(dpi=600)
-            pil_image = Image.open(io.BytesIO(rgb.tobytes()))
+        npcs, allnobs = d['pcs'].shape
+        nobs = allnobs // ntravis
 
-            axs[k].imshow(pil_image.convert('RGB'))
-            axs[k].axis('off')
-            put_panel_label(axs[k], k)
-            k += 1
-            row += 1
+        for j in range(ntravis):
 
-        '''
-        Trajectories for example regions in PCA embedded 3d space
-        '''
+            # 3d trajectory
+            cs = d['pcs'][:, nobs * j: nobs * (j + 1)].T
 
-        c = 0
-        for split in align:
-
-            if ga_pcs:
-                d = np.load(Path(pth_res, '{split}_grand_averages.npy'),
-                            allow_pickle=True).flat[0]
+            if j == 0:
+                col = grad('Blues_r', nobs)
+            elif j == 1:
+                col = grad('Reds_r', nobs)
             else:
-                d = np.load(Path(pth_res, f'{split}.npy'),
-                            allow_pickle=True).flat[0][split]
+                col = grad('Greys_r', nobs)
 
-                # pick example region
-                reg = exs[split][0]
+            axs[k].plot(cs[:, 0], cs[:, 1], cs[:, 2],
+                        color=col[len(col) // 2],
+                        linewidth=5 if j in [0, 1] else 1,
+                        label=trial_split[split][j] if j in range(3)
+                        else '_nolegend_', alpha=0.5)
 
-            if only3d:
-                axs.append(fig.add_subplot(gs[:, c],
-                                           projection='3d'))
-            else:
-                axs.append(fig.add_subplot(gs[0 - v:4 - v, c],
-                                           projection='3d'))
+            axs[k].scatter(cs[:, 0], cs[:, 1], cs[:, 2],
+                           color=col,
+                           edgecolors=col,
+                           s=20 if j in [0, 1] else 1,
+                           depthshade=False)
 
-            npcs, allnobs = d['pcs'].shape
-            nobs = allnobs // ntravis
+        axs[k].set_title(f"{split}, {reg} {d[reg]['nclus']}"
+                         if not ga_pcs else split)
+        axs[k].grid(False)
+        axs[k].axis('off')
 
-            for j in range(ntravis):
 
-                # 3d trajectory
-                cs = d['pcs'][:, nobs * j: nobs * (j + 1)].T
+        put_panel_label(axs[k], k)
 
-                if j == 0:
-                    col = grad('Blues_r', nobs)
-                elif j == 1:
-                    col = grad('Reds_r', nobs)
-                else:
-                    col = grad('Greys_r', nobs)
-
-                axs[k].plot(cs[:, 0], cs[:, 1], cs[:, 2],
-                            color=col[len(col) // 2],
-                            linewidth=5 if j in [0, 1] else 1,
-                            label=trial_split[split][j] if j in range(3)
-                            else '_nolegend_', alpha=0.5)
-
-                axs[k].scatter(cs[:, 0], cs[:, 1], cs[:, 2],
-                               color=col,
-                               edgecolors=col,
-                               s=20 if j in [0, 1] else 1,
-                               depthshade=False)
-
-            axs[k].set_title(f"{split}, {reg} {d[reg]['nclus']}"
-                             if not ga_pcs else split)
-            axs[k].grid(False)
-            axs[k].axis('off')
-
-            if c == 0:
-                put_panel_label(axs[k], row)
-
-            c += 1
-            k += 1
-
+        k += 1
         row += 1
 
-        if only3d:
-            return
 
-        '''
-        line plot per 5 example regions per split
-        '''
-        c = 0  # column index
+    '''
+    line plot per 5 example regions per split
+    '''
+    row = 0  # index
 
-        for split in align:
-            if c == 0:
-                axs.append(fig.add_subplot(gs[4 - v:6 - v, c]))
-                # axs[-1].set_ylim(0, 4.5/b_size)
-            else:  # to share y axis
-                axs.append(fig.add_subplot(gs[4 - v:6 - v, c]))
-                # ,sharey=axs[len(axs)-1]))
+    for split in splits:
 
-            d = np.load(Path(pth_res, f'{split}.npy'),
-                        allow_pickle=True).flat[0]
-
-            # example regions to illustrate line plots
-            regs = exs[split]
-
-            texts = []
-            for reg in regs:
-                if any(np.isinf(d[reg][f'd_{curve}'])):
-                    print(f'inf in {curve} of {reg}')
-                    continue
-
-                xx = np.linspace(-pre_post[split][0],
-                                 pre_post[split][1],
-                                 len(d[reg][f'd_{curve}']))
-
-                # get units in Hz
-                yy = d[reg][f'd_{curve}']
-
-                axs[k].plot(xx, yy, linewidth=2,
-                            color=palette[reg],
-                            label=f"{reg} {d[reg]['nclus']}")
+        axs.append(fig.add_subplot(gs[row,3:6]))
 
 
-#                # plot stderror bars on lines
-#                axs[k].fill_between(xx,
-#                                 yy + d[reg][f'stde_{curve}'],
-#                                 yy - d[reg][f'stde_{curve}'],
-#                                 color=palette[reg],
-#                                 alpha = 0.2)
+        d = np.load(Path(pth_res, f'{split}.npy'),
+                    allow_pickle=True).flat[0]
 
-                # put region labels
-                y = yy[-1]
-                x = xx[-1]
-                ss = f"{reg} {d[reg]['nclus']}"
+        # example regions to illustrate line plots
+        regs = exs[split]
 
-                if cosregs[reg] in ['CBX', 'CBN']:  # darken yellow
-                    texts.append(axs[k].text(x, y, ss,
-                                             color='k',
-                                             fontsize=8))
+        texts = []
+        for reg in regs:
+            if any(np.isinf(d[reg][f'd_{curve}'])):
+                print(f'inf in {curve} of {reg}')
+                continue
 
+            xx = np.linspace(-pre_post[split][0],
+                             pre_post[split][1],
+                             len(d[reg][f'd_{curve}']))
+
+            # get units in Hz
+            yy = d[reg][f'd_{curve}']
+
+            axs[k].plot(xx, yy, linewidth=2,
+                        color=palette[reg],
+                        label=f"{reg} {d[reg]['nclus']}")
+
+            # put region labels
+            y = yy[-1]
+            x = xx[-1]
+            ss = f"{reg} {d[reg]['nclus']}"
+
+            if cosregs[reg] in ['CBX', 'CBN']:  # darken yellow
                 texts.append(axs[k].text(x, y, ss,
-                                         color=palette[reg],
+                                         color='k',
                                          fontsize=8))
 
-            axs[k].axvline(x=0, lw=0.5, linestyle='--', c='k')
+            texts.append(axs[k].text(x, y, ss,
+                                     color=palette[reg],
+                                     fontsize=8))
 
-            if split in ['block', 'choice']:
-                ha = 'left'
-            else:
-                ha = 'right'
+        axs[k].axvline(x=0, lw=0.5, linestyle='--', c='k')
 
-            axs[k].spines['top'].set_visible(False)
-            axs[k].spines['right'].set_visible(False)
-            if c == 0:
-                axs[k].set_ylabel('distance [Hz]')
-            axs[k].set_xlabel('time [sec]')
+        if split in ['block', 'choice']:
+            ha = 'left'
+        else:
+            ha = 'right'
 
-            if c == 0:
-                put_panel_label(axs[k], row)
+        axs[k].spines['top'].set_visible(False)
+        axs[k].spines['right'].set_visible(False)
+        
+        axs[k].set_ylabel('distance [Hz]')
+        axs[k].set_xlabel('time [sec]')
 
-            c += 1
-            k += 1
+        
+        put_panel_label(axs[k], k)
 
         row += 1
-
-    if onlyScat:
-        v = 8
+        k += 1
 
     '''
     scatter latency versus max amplitude for significant regions
     '''
 
-    fsize = 20 if single_scat else 9 if onlyScat else 7
-    dsize = 120 if single_scat else 9 if onlyScat else 4  # was 1
+    fsize = 7
+    dsize = 4  
 
     if amp_number:
         fig2 = plt.figure()
         axss = []
         fig2.suptitle(f'distance metric: {curve}')
 
-    if single_scat:
-        figs = [plt.subplots(figsize=(10, 10))
-                for split in align]
 
-    c = 0  # column idx
-    r = 0  # row idx
+    row = 0  # row idx
     
-    for split in align:
+    for split in splits:
 
-        if not single_scat:
-
-            axs.append(fig.add_subplot(gs[6 +r : 6 +r +2, :]))
-
-        else:
-            axs.append(figs[c][1])
-            axs[-1].set_ylim(0, 7.5 / b_size if split ==
-                             'fback' else 4.5 / b_size)
-            axs[-1].xaxis.set_major_locator(ticker.MultipleLocator(0.05))
+        axs.append(fig.add_subplot(gs[row ,6:]))
 
         d = np.load(Path(pth_res, f'{split}.npy'),
                     allow_pickle=True).flat[0]
@@ -1208,7 +1141,7 @@ def plot_all(curve='euc', amp_number=False, intro=True,
         cols = [palette[reg] for reg in acronyms]
 
         if amp_number:  # supp figure for correlation of nclus and maxes
-            axss.append(fig2.add_subplot(int(f'1{len(align)}{c+1}')))
+            axss.append(fig2.add_subplot(int(f'1{len(splits)}{c+1}')))
             nums = [1 / d[reg]['nclus'] for reg in np.array(acronyms)[ac_sig]]
 
             ll = list(zip(nums, np.array(maxes)[ac_sig],
@@ -1234,13 +1167,6 @@ def plot_all(curve='euc', amp_number=False, intro=True,
                        color=np.array(cols)[~ac_sig],
                        marker='o', s=dsize / 10)
 
-#        # put extra marker for highlighted regions
-#        exs_i = [i for i in range(len(acronyms))
-#                 if acronyms[i] in exs[split]]
-
-#        axs[k].scatter(np.array(lats)[exs_i], np.array(maxes)[exs_i],
-#                       color=np.array(cols)[exs_i], marker='x',s=10*dsize)
-
         texts = []
         alw = 0.1
         for i in range(len(acronyms)):
@@ -1255,10 +1181,7 @@ def plot_all(curve='euc', amp_number=False, intro=True,
                              maxes[i]),
                             fontsize=fsize,
                             color='k',
-                            arrowprops=dict(
-                                arrowstyle="-",
-                                color='k',
-                                lw=alw) if auto_label_adjust else None))
+                            arrowprops=None))
 
                 else:
                     texts.append(
@@ -1267,13 +1190,8 @@ def plot_all(curve='euc', amp_number=False, intro=True,
                             (lats[i], maxes[i]),
                             fontsize=fsize,
                             color=palette[acronyms[i]],
-                            arrowprops=dict(
-                                arrowstyle=" ",
-                                color=palette[acronyms[i]], lw=alw)
-                            if auto_label_adjust else None))
+                            arrowprops=None))
 
-        if auto_label_adjust:
-            adjust_text(texts, force_text=(3, 6), ax=axs[k])
 
         axs[k].axvline(x=0, lw=0.5, linestyle='--', c='k')
 
@@ -1286,37 +1204,19 @@ def plot_all(curve='euc', amp_number=False, intro=True,
 
         axs[k].spines['top'].set_visible(False)
         axs[k].spines['right'].set_visible(False)
-        if c == 0:
-            axs[k].set_ylabel('max dist. [Hz]')
+        
+        axs[k].set_ylabel('max dist. [Hz]')
         axs[k].set_xlabel('latency [sec]')
         axs[k].set_title(f"{tops[split+'_s']} sig")
 
-        if c == 0:
-            put_panel_label(axs[k], row)
+        
+        put_panel_label(axs[k], k)
 
-        if single_scat:
-            figs[c][0].tight_layout()
-            figs[c][0].savefig(Path(pth_res,
-                               f'scat_{split}.pdf'), dpi=300)
-            plt.close()
-
-        c += 1
+        row += 1
         k += 1
-        r += 2
 
     fig = plt.gcf()
-#    fig.tight_layout()
-#
-    fig.subplots_adjust(top=0.98,
-                        bottom=0.051,
-                        left=0.06,
-                        right=0.98,
-                        hspace=5.1,
-                        wspace=0.62)
-
-    # fig.suptitle(f'd_{curve}')
-
-#    fig.savefig(Path(pth_res,f'manifold_{curve}.pdf'), dpi=400)
+    fig.tight_layout()
 
 
 def plot_cosmos_lines(curve='euc', single=True,
@@ -1328,7 +1228,7 @@ def plot_cosmos_lines(curve='euc', single=True,
         splits = align
 
     df, palette = get_allen_info()
-    fig = plt.figure(figsize=(20, 15), constrained_layout=True)
+    fig = plt.figure(figsize=(20, 15))
 
     gs = GridSpec(len(splits), 7, figure=fig)
 
@@ -1405,14 +1305,7 @@ def plot_cosmos_lines(curve='euc', single=True,
 
                 cc += 1
 
-            # adjust_text(texts)
 
-
-#            axs[k].axvline(x=0, lw=0.5, linestyle='--', c='k')
-#            axs[k].text(0, 0.01, align[split],
-#                          transform=axs[k].get_xaxis_transform(),
-#                          horizontalalignment = 'right' if split == 'block'
-#                          else 'left')
             axs[k].spines['top'].set_visible(False)
             axs[k].spines['right'].set_visible(False)
             axs[k].set_ylabel('mean firing rate' if licka else 'distance')
@@ -1423,12 +1316,6 @@ def plot_cosmos_lines(curve='euc', single=True,
 
             k += 1
         sc += 1
-
-
-#    if single:
-#        fig.savefig(Path(pth_res,f'lines_{curve}_{ssplit}.png'))
-#    else:
-#        fig.savefig(Path(pth_res,f'lines_{curve}_.png'))
 
 
 def plot_session_numbers():
