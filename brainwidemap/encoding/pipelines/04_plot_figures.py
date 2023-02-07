e1b0e33fa139b4fd2852322962ce18d7c04ffc16
Berk Gercek <gercek.berk@gmail.com>
2023-02-07 12:30:36 +0300

    Updated plotting

--- brainwidemap/encoding/pipelines/04_plot_figures.py
+++ brainwidemap/encoding/pipelines/04_plot_figures.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 # Third party libraries
 import matplotlib.pyplot as plt
+from matplotlib import colors
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -39,12 +40,13 @@ COLOR_MAPS = {
     "pLeft_tr": "Blues",
     "wheel": "Purples",
 }
-COLOR_RANGE = [0.0, 0.5]
+COLOR_RANGE = [5, 95]
+GLOBAL_CMAP = True
 ANNOTATE = False
 IMGFMT = "pdf"
+SAVEPATH = "/home/berk/Documents/Projects/results/plots/"
 
 fitfile = Path(GLM_FIT_PATH).joinpath(FITDATE + "_glm_fit.pkl")
-fitfolder = Path(GLM_FIT_PATH).joinpath("merged_results").joinpath(FITDATE)
 
 if not fitfile.exists():
     raise FileNotFoundError(f"Fit folder {fitfile} does not exist")
@@ -52,20 +54,23 @@ if not fitfile.exists():
 br = BrainRegions()
 
 
-def flatmap_variable(df, cmap):
+def flatmap_variable(df, cmap, cmin=COLOR_RANGE[0], cmax=COLOR_RANGE[1]):
     fig = plt.figure(figsize=(8, 4) if not ANNOTATE else (16, 8))
     ax = fig.add_subplot(111)
-    ax = plot_swanson(
-        df.index,
-        df.values,
-        hemisphere="left",
-        cmap=cmap,
-        br=br,
-        ax=ax,
-        annotate=ANNOTATE,
-        vmin=COLOR_RANGE[0],
-        vmax=COLOR_RANGE[1],
-    )
+    cmap_kwargs = {
+        "vmin": cmin,
+        "vmax": cmax
+    } if not GLOBAL_CMAP else {
+        "norm": colors.LogNorm(vmin=cmin, vmax=cmax, clip=True)
+    }
+    ax = plot_swanson(df.index,
+                      df.values,
+                      hemisphere="left",
+                      cmap=cmap,
+                      br=br,
+                      ax=ax,
+                      annotate=ANNOTATE,
+                      **cmap_kwargs)
     fig.subplots_adjust(right=0.85)
     cb_ax = fig.add_axes([0.88, 0.25, 0.02, 0.5])
     plt.colorbar(mappable=ax.images[0], cax=cb_ax)
@@ -79,9 +84,9 @@ fitdata = pd.read_pickle(fitfile)
 # Distribution of full model R^2 values, and std. dev between folds
 meanscores = fitdata["mean_fit_results"]
 full_model = meanscores["full_model"].copy()
-full_model_std = (
-    fitdata["fit_results"].groupby(["eid", "pid", "clu_id"]).agg({"full_model": "std"})
-)
+full_model_std = (fitdata["fit_results"].groupby(["eid", "pid", "clu_id"
+                                                  ]).agg({"full_model":
+                                                          "std"}))
 joindf = full_model_std.join(full_model, how="inner", lsuffix="_std")
 sns.histplot(
     data=joindf,
@@ -98,23 +103,31 @@ ax = plt.gca()
 ax.set_xlim(-0.015, 0.03)
 plt.tight_layout()
 
-
+unitcounts = meanscores.groupby("region").size().astype(int)
+keepreg = unitcounts[unitcounts >= MIN_UNITS].index
+if GLOBAL_CMAP:
+    allmeans = meanscores.set_index(
+        "region", append=True)[VARIABLES].groupby("region").mean()
+    cmin = np.percentile(allmeans.values.flatten(), COLOR_RANGE[0])
+    if cmin < 0:
+        cmin = 1e-5
+    cmax = np.percentile(allmeans.values.flatten(), COLOR_RANGE[1])
 for variable in VARIABLES:
-    var_percentiles = pd.read_parquet(fitfile.joinpath(f"{variable}_percentiles.parquet"))
-    nfolds = var_percentiles.groupby("fold").ngroups
-    unitcounts = (var_percentiles.groupby("region").size() / nfolds).astype(int)
-    keepreg = unitcounts[unitcounts >= MIN_UNITS].index
-    mask = var_percentiles.index.isin(keepreg, level="region")
-    var_percentiles = var_percentiles.loc[:, :, :, mask, :]
-    twotail = np.abs(var_percentiles - 0.5) * 2
-    meanp = twotail.groupby(["eid", "probe", "clu_id", "region"]).mean()
-    corr_alpha = ALPHA / len(var_percentiles.columns)  # Bonferroni correction
-    significance = np.any(meanp >= (1 - corr_alpha), axis=1)
-    propsig = significance.groupby("region").mean()
-    fig, ax, cb_ax = flatmap_variable(propsig, COLOR_MAPS[variable])
+    varscores = meanscores.set_index("region", append=True)[variable]
+    mask = varscores.index.isin(keepreg, level="region")
+    varscores = varscores.loc[:, :, :, mask, :]
+    regmeans = varscores.groupby("region").mean()
+    if not GLOBAL_CMAP:
+        cmin = np.percentile(regmeans, 0)
+        cmax = np.percentile(regmeans, 100)
+    fig, ax, cb_ax = flatmap_variable(regmeans, COLOR_MAPS[variable], cmin,
+                                      cmax)
     fig.suptitle(f"{variable} significance levels")
     fig.savefig(
-        fitfolder.joinpath(f"{variable}_significance{'_annotated' * ANNOTATE}.{IMGFMT}"),
+        SAVEPATH.joinpath(
+            f"{variable}_significance{'_annotated' * ANNOTATE}{'_global_cmap' * GLOBAL_CMAP}.{IMGFMT}"
+        ),
         format=IMGFMT,
-        dpi=600,
+        dpi=450,
     )
+    plt.close()
