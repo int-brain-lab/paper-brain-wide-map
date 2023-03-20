876fee5f3c70f2ca96f92a139ecc09bb5d2c2a25
Berk Gercek <gercek.berk@gmail.com>
2023-03-20 13:02:20 +0100

    Block distribution plots

--- brainwidemap/encoding/scripts/twocond_plots.py
+++ brainwidemap/encoding/scripts/twocond_plots.py
@@ -1,18 +1,21 @@
-from brainwidemap.bwm_loading import bwm_query
 from pathlib import Path
-from brainwidemap.encoding.glm_predict import GLMPredictor
+
+import brainwidemap.encoding.cluster_worker as cw
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from brainwidemap.bwm_loading import bwm_query
+from brainwidemap.encoding.glm_predict import GLMPredictor, predict
+from brainwidemap.encoding.params import GLM_CACHE, GLM_FIT_PATH
+
 import neurencoding.linear as lm
 from neurencoding.utils import remove_regressors
-import matplotlib.pyplot as plt
-import brainwidemap.encoding.cluster_worker as cw
-import pandas as pd
-import numpy as np
-from brainwidemap.encoding.params import GLM_FIT_PATH, GLM_CACHE
 
 # Please use the saved parameters dict form 02_fit_sessions.py as params
 PLOTPATH = Path("/home/berk/Documents/Projects/results/plots/prediction_summaries")
 N_TOP_UNITS = 5
-IMGFMT = "png"
+IMGFMT = "svg"
 alignsets = {  # Sets of align_time as key with aligncol, aligncond1/2 functions, and t_before/t_after as the values
     "stimOn_times": (
         "contrastRight",
@@ -47,6 +50,8 @@ targetreg = {  # Function to produce the target metric, the target regions, and 
     "stim": (lambda df: df["stimonR"] - df["stimonL"], ["VISp"], "stimOn_times"),
     "choice": (lambda df: df["fmoveR"] - df["fmoveL"], ["GRN"], "firstMovement_times"),
     "feedback": (lambda df: df["correct"] - df["incorrect"], ["IRN"], "feedback_times"),
+    "wheel": (lambda df: df["wheel"], ["GRN"], "firstMovement_times"),
+    "block": (lambda df: df["pLeft"], ["PL"], "stimOn_times")
 }
 
 params = pd.read_pickle(GLM_FIT_PATH + "/2023-03-07_glm_fit_pars.pkl")
@@ -148,9 +153,12 @@ for variable, (targetmetricfun, regions, aligntime) in targetreg.items():
                 aligncond2,
                 t_before,
                 t_after,
-                [reg1, reg2],
+                [reg1, reg2] if variable != "wheel" else ["wheel"],
             )
-            remstr = f"\n[{reg1}, {reg2}] regressors rem."
+            if variable != "wheel":
+                remstr = f"\n[{reg1}, {reg2}] regressors rem."
+            else:
+                remstr = "\nwheel regressor rem."
             names = [reg1, reg2, reg1 + remstr, reg2 + remstr]
             for subax, title in zip(ax[0, :], names):
                 subax.set_title(title)
@@ -160,3 +168,54 @@ for variable, (targetmetricfun, regions, aligntime) in targetreg.items():
                 )
             )
             plt.close()
+
+## Treat block separately since it's a different type of plot
+variable = "block"
+targetmetricfun, regions, aligntime = targetreg["block"]
+varfolder = Path(PLOTPATH).joinpath(variable)
+if not varfolder.exists():
+    varfolder.mkdir()
+targetmetric = targetmetricfun(meanscores)
+for region in regions:
+    topunits = (
+        targetmetric.loc[:, :, :, region].sort_values(ascending=False).iloc[:N_TOP_UNITS]
+    )
+    for (eid, pid, clu_id), drsq in topunits.items():
+        sessdf = bwm_query()
+        subject = sessdf[sessdf["eid"] == eid]["subject"].iloc[0]
+        eidfn = Path(GLM_CACHE).joinpath(Path(f"{subject}/{eid}/2022-12-22_{pid}_regressors.pkl"))
+        stdf, sspkt, sspkclu, sclureg, clu_df = cw.get_cached_regressors(eidfn)
+        design = cw.generate_design(stdf, stdf["probabilityLeft"], t_before=0.6, **params)
+        spkmask = sspkclu == clu_id
+        nglm = lm.LinearGLM(
+            design, sspkt[spkmask], sspkclu[spkmask], estimator=params["estimator"], mintrials=0
+        )
+        nglm.fit()
+        pred, trlabels = predict(nglm, glm_type="linear", retlab=True)
+        mask = design.dm[:, design.covar["pLeft"]["dmcol_idx"]] != 0
+        itipred = pred[clu_id][mask]
+        iticounts = nglm.binnedspikes[mask, :]
+        labels = trlabels[mask]
+        rates = pd.DataFrame(index=stdf.index[stdf.probabilityLeft != 0.5], columns=["firing_rate", "pred_rate", "pLeft"], dtype=float)
+        for p_val in [0.2, 0.8]:
+            trials = stdf.index[stdf.probabilityLeft == p_val]
+            for trial in trials:
+                trialmask = labels == trial
+                rates.loc[trial, "firing_rate"] = np.mean(iticounts[trialmask]) / design.binwidth
+                rates.loc[trial, "pred_rate"] = np.mean(itipred[trialmask]) / design.binwidth
+                rates.loc[trial, "pLeft"] = p_val
+        fig, ax = plt.subplots(1, 2, figsize=(6, 6), sharey=True)
+        sns.boxplot(rates, x="pLeft", y="firing_rate", ax=ax[0])
+        sns.boxplot(rates, x="pLeft", y="pred_rate", ax=ax[1])
+        ax[0].set_title(f"{region} {clu_id} firing rate by block")
+        ax[1].set_title(f"{region} {clu_id} predicted rate by block")
+        ax[0].set_ylabel("Firing rate (spikes/s)")
+        plt.savefig(
+            varfolder.joinpath(
+                f"{eid}_{pid}_clu{clu_id}_{region}_{variable}_predsummary.{IMGFMT}"
+            )
+        )
+        plt.close()
+
+
+
