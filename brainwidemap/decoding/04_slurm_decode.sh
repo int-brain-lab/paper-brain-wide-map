ba3019c15c9669068cc8c2a3d76de97450ea0387
bensonbrandon <bensonb42@gmail.com>
2023-03-27 14:36:27 -0700

    debug jan 18, new code used

--- brainwidemap/decoding/04_slurm_decode.sh
+++ brainwidemap/decoding/04_slurm_decode.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=decoding
-#SBATCH --output=/scratch/users/bensonb/international-brain-lab/paper-brain-wide-map/brainwidemap/logs/slurm/df_bwmmar_09_6_.%a.out
-#SBATCH --error=/scratch/users/bensonb/international-brain-lab/paper-brain-wide-map/brainwidemap/logs/slurm/df_bwmmar_09_6_.%a.err
+#SBATCH --output=/scratch/users/bensonb/international-brain-lab/paper-brain-wide-map/brainwidemap/logs/slurm/dc_debugjan18_new_1_.%a.out
+#SBATCH --error=/scratch/users/bensonb/international-brain-lab/paper-brain-wide-map/brainwidemap/logs/slurm/dc_debugjan18_new_1_.%a.err
 #SBATCH --partition=normal
-#SBATCH --array=1001-1100
+#SBATCH --array=1-1000
 #SBATCH --mem=16G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=bensonb@stanford.edu
-#SBATCH --time=24:00:00
+#SBATCH --time=12:00:00
 
 #extracting settings from $SLURM_ARRAY_TASK_ID
 echo slurm_task $SLURM_ARRAY_TASK_ID
@@ -17,10 +17,10 @@ export PYTHONPATH="$PWD":$PYTHONPATH
 
 echo
 # change to conda  => which python
-python 04_decode_single_session.py $SLURM_ARRAY_TASK_ID
+#python 04_decode_single_session.py $SLURM_ARRAY_TASK_ID
 
 # example of running a subset of the BWM dataset with only the given subjects 
 # You can add any number of subjects to filter the BWM dataset: 10 are shown
 #python 04_decode_single_session.py $SLURM_ARRAY_TASK_ID DY_011 CSHL059 SWC_054 KS042 ZFM-02373 ibl_witten_26 CSH_ZAD_029 UCLA037 SWC_038 NYU-39
-
+python 04_decode_single_session.py $SLURM_ARRAY_TASK_ID DY_011 CSHL059 SWC_054
 
