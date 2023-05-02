78060582143c64c3a3b71c240c3e71b38de40ecb
juhuntenburg <ju.huntenburg@gmail.com>
2023-05-02 13:15:50 -0400

    remove many irrelevant files

--- brainwidemap/encoding/params.py
+++
@@ -1,8 +0,0 @@
-"""
-This file is referenced for storing cached data from the brain-wide map and the outputs of fits.
-Users must change these paths to valid locations on their own system for most of the code base to
-work.
-"""
-
-GLM_CACHE = "/mnt/Storage/glm_cache/"
-GLM_FIT_PATH = "/mnt/Storage/results/glms/"
