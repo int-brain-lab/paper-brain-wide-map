2f378d739ae97495b6b1c1b3e746d2f47834c157
Gaelle <gaelle.chapuis.ibl@gmail.com>
2023-05-25 10:36:16 +0200

    mean and median for weight and age

--- brainwidemap/meta/mice_info.py
+++ brainwidemap/meta/mice_info.py
@@ -9,17 +9,18 @@ electrophysiological recording.
 # Author: Gaelle Chapuis
 from brainwidemap import bwm_query
 from one.api import ONE
+import numpy as np
 
 one = ONE()
-bwm_df, insertions = bwm_query(one, return_details=True)
+bwm_df = bwm_query(one, return_details=True)
 
 
 sub_list = list()
 id_subj_all = list()
-for q_i in insertions:
+for index, row in bwm_df.iterrows():
     subj = one.alyx.rest('subjects', 'list',
-                         nickname=q_i['session_info']['subject'],
-                         lab=q_i['session_info']['lab'])[0]
+                         nickname=row['subject'],
+                         lab=row['lab'])[0]
     id_subj = subj['id']
     if id_subj not in id_subj_all:  # Remove duplicates if any
         sub_list.append(subj)
@@ -30,11 +31,15 @@ print(f'N subject: {len(sub_list)}')
 
 # AGE RANGE
 age_weeks = [item['age_weeks'] for item in sub_list if item['age_weeks'] != 0]
-print(f'AGE RANGE: {min(age_weeks)}-{max(age_weeks)} weeks')
+print(f'AGE RANGE: {min(age_weeks)}-{max(age_weeks)} weeks; '
+      f'mean {round(np.mean(age_weeks), 2)} weeks, '
+      f'median {round(np.median(age_weeks), 2)} weeks')
 
 # WEIGHT RANGE
 ref_weight = [item['reference_weight'] for item in sub_list]
-print(f'WEIGHT RANGE: {min(ref_weight)}-{max(ref_weight)} g')
+print(f'WEIGHT RANGE: {min(ref_weight)}-{max(ref_weight)} g; '
+      f'mean {round(np.mean(ref_weight), 2)} g, '
+      f'median {round(np.median(ref_weight), 2)} g')
 
 # Age 0
 age_0 = [item['nickname'] for item in sub_list if item['age_weeks'] == 0]
