051984ea3125db7e73a3d3c57e724ddb6e582dfc
Berk Gercek <gercek.berk@gmail.com>
2023-06-16 10:57:41 +0200

    Refactor subpanel_plots

--- brainwidemap/encoding/scripts/subpanel_plots.py
+++ brainwidemap/encoding/scripts/subpanel_plots.py
@@ -1,89 +1,18 @@
 from pathlib import Path
 
-import brainwidemap.encoding.cluster_worker as cw
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from one.api import ONE
 from brainwidemap.bwm_loading import bwm_query
+from brainwidemap.encoding.design import generate_design
 from brainwidemap.encoding.glm_predict import GLMPredictor, predict
 from brainwidemap.encoding.params import GLM_CACHE
-from brainwidemap.encoding.utils import single_cluster_raster, find_trial_ids
+from brainwidemap.encoding.utils import load_regressors, single_cluster_raster, find_trial_ids
 
 import neurencoding.linear as lm
 from neurencoding.utils import remove_regressors
-
-# Please use the saved parameters dict from 02_fit_sessions.py as params
-PLOTPATH = Path("/home/berk/Documents/Projects/results/plots/prediction_summaries")
-N_TOP_UNITS = 20
-RAST_BINSIZE = 0.002
-plt.rcParams["svg.fonttype"] = "none"
-alignsets = {  # Sets of align_time as key with aligncol, aligncond1/2 functions, and t_before/t_after as the values
-    "stimOn_times": (
-        "contrastRight",
-        lambda c: np.isnan(c),
-        lambda c: np.isfinite(c),
-        0.1,
-        0.4,
-        "stimonL",
-        "stimonR",
-    ),
-    "firstMovement_times": (
-        "choice",
-        lambda c: c == 1,
-        lambda c: c == -1,
-        0.2,
-        0.05,
-        "fmoveL",
-        "fmoveR",
-    ),
-    "feedback_times": (
-        "feedbackType",
-        lambda f: f == 1,
-        lambda f: f == -1,
-        0.1,
-        0.4,
-        "correct",
-        "incorrect",
-    ),
-}
-
-glm_params = pd.read_pickle(Path(__file__).parent.joinpath("glm_params.pkl"))
-
-targetunits = {  # eid, pid, clu_id, region, drsq, alignset key
-    "stim": (
-        "e0928e11-2b86-4387-a203-80c77fab5d52",  # EID
-        "799d899d-c398-4e81-abaf-1ef4b02d5475",  # PID
-        209,  # clu_id
-        "VISp", # region
-        0.04540706, # drsq
-        "stimOn_times",  # Alignset key
-    ),
-    "choice": (
-        "671c7ea7-6726-4fbe-adeb-f89c2c8e489b",
-        "04c9890f-2276-4c20-854f-305ff5c9b6cf",
-        123,
-        "GRN",
-        0.000992895, # drsq
-        "firstMovement_times",
-    ),
-    "feedback": (
-        "a7763417-e0d6-4f2a-aa55-e382fd9b5fb8",
-        "57c5856a-c7bd-4d0f-87c6-37005b1484aa",
-        98,
-        "IRN",
-        0.3077195113, # drsq
-        "feedback_times",
-    ),
-    "block": (
-        "7bee9f09-a238-42cf-b499-f51f765c6ded",
-        "26118c10-35dd-4ab1-9f0f-b9a89a1da070",
-        207,
-        "MOp",
-        0.0043285, # drsq
-        "stimOn_times",
-    ),
-}
 
 
 def plot_twocond(
@@ -98,16 +27,7 @@ def plot_twocond(
     t_after,
     regressors,
 ):
-    sessdf = bwm_query()
-    subject = sessdf[sessdf["eid"] == eid]["subject"].iloc[0]
-    eidfn = Path(GLM_CACHE).joinpath(Path(f"{subject}/{eid}/2022-12-22_{pid}_regressors.pkl"))
-    stdf, sspkt, sspkclu, sclureg, clu_df = cw.get_cached_regressors(eidfn)
-    design = cw.generate_design(stdf, stdf["probabilityLeft"], t_before=0.6, **glm_params)
-    spkmask = sspkclu == clu_id
-    nglm = lm.LinearGLM(
-        design, sspkt[spkmask], sspkclu[spkmask], estimator=glm_params["estimator"], mintrials=0
-    )
-    nglm.fit()
+    stdf, sspkt, sspkclu, design, spkmask, nglm = load_unit_fit_model(eid, pid, clu_id)
     pred = GLMPredictor(stdf, nglm, sspkt, sspkclu)
     fig, ax = plt.subplots(3, 4, figsize=(12, 12), sharey="row")
     oldticks = []
@@ -158,17 +78,120 @@ def plot_twocond(
     return fig, ax, sspkt, sspkclu, stdf
 
 
+def load_unit_fit_model(eid, pid, clu_id):
+    stdf, sspkt, sspkclu, _, __ = load_regressors(
+        eid,
+        pid,
+        one,
+        t_before=0.6,
+        t_after=0.6,
+        binwidth=glm_params["binwidth"],
+        abswheel=True,
+    )
+    design = generate_design(stdf, stdf["probabilityLeft"], t_before=0.6, **glm_params)
+    spkmask = sspkclu == clu_id
+    nglm = lm.LinearGLM(
+        design, sspkt[spkmask], sspkclu[spkmask], estimator=glm_params["estimator"], mintrials=0
+    )
+    nglm.fit()
+    return stdf, sspkt, sspkclu, design, spkmask, nglm
+
+
+# Please use the saved parameters dict from 02_fit_sessions.py as params
+PLOTPATH = Path("/home/berk/Documents/Projects/results/plots/prediction_summaries")
+N_TOP_UNITS = 20
+RAST_BINSIZE = 0.002
+one = ONE()
+plt.rcParams["svg.fonttype"] = "none"
+# Sets of align_time as key with aligncol, aligncond1/2 functions, t_before/t_after,
+# and the name of the associated model regressors as values
+alignsets = {
+    "stimOn_times": (
+        "contrastRight",  # Column name in df to use for filtering
+        lambda c: np.isnan(c),  # Condition 1 function (left stim)
+        lambda c: np.isfinite(c),  # Condition 2 function (right stim)
+        0.1,  # Time before align_time to include in trial psth/raster
+        0.4,  # Time after align_time to include in trial psth/raster
+        "stimonL",  # Condition 1 label within the GLM design matrix
+        "stimonR",  # Condition 2 label within the GLM design matrix
+    ),
+    "firstMovement_times": (
+        "choice",
+        lambda c: c == 1,
+        lambda c: c == -1,
+        0.2,
+        0.05,
+        "fmoveL",
+        "fmoveR",
+    ),
+    "feedback_times": (
+        "feedbackType",
+        lambda f: f == 1,
+        lambda f: f == -1,
+        0.1,
+        0.4,
+        "correct",
+        "incorrect",
+    ),
+}
+
+glm_params = pd.read_pickle(Path(__file__).parent.joinpath("glm_params.pkl"))
+
+# Which units we're going to use for plotting
+targetunits = {  # eid, pid, clu_id, region, drsq, alignset key
+    "stim": (
+        "e0928e11-2b86-4387-a203-80c77fab5d52",  # EID
+        "799d899d-c398-4e81-abaf-1ef4b02d5475",  # PID
+        209,  # clu_id
+        "VISp",  # region
+        0.04540706,  # drsq (from 02_fit_sessions.py)
+        "stimOn_times",  # Alignset key
+    ),
+    "choice": (
+        "671c7ea7-6726-4fbe-adeb-f89c2c8e489b",
+        "04c9890f-2276-4c20-854f-305ff5c9b6cf",
+        123,
+        "GRN",
+        0.000992895,  # drsq
+        "firstMovement_times",
+    ),
+    "feedback": (
+        "a7763417-e0d6-4f2a-aa55-e382fd9b5fb8",
+        "57c5856a-c7bd-4d0f-87c6-37005b1484aa",
+        98,
+        "IRN",
+        0.3077195113,  # drsq
+        "feedback_times",
+    ),
+    "block": (
+        "7bee9f09-a238-42cf-b499-f51f765c6ded",
+        "26118c10-35dd-4ab1-9f0f-b9a89a1da070",
+        207,
+        "MOp",
+        0.0043285,  # drsq
+        "stimOn_times",
+    ),
+}
+
+
+# This is a hack to use the "find trial IDs" function provided by Mayo to get the trial IDs
+# corresponding to each condition for the different variables
 sortlookup = {"stim": "side", "choice": "movement", "feedback": "fdbk", "wheel": "movement"}
 
 for variable, (eid, pid, clu_id, region, drsq, aligntime) in targetunits.items():
+    # Skip block (separate logic) and make folder structure before continuing if folders don't exist
     if variable == "block":
         continue
-    varfolder = Path(PLOTPATH).joinpath(variable)
-    rasterfolder = varfolder.joinpath("rasters")
+    varfolder = Path(PLOTPATH).joinpath(variable)  # Variable gets its own folder
+    rasterfolder = varfolder.joinpath("rasters")  # Rasters in a separate subfolder for variable
     if not varfolder.exists():
         varfolder.mkdir()
     if not rasterfolder.exists():
         rasterfolder.mkdir()
+    if not varfolder.joinpath("png").exists():  # PNGs separated too
+        varfolder.joinpath("png").mkdir()
+    if not rasterfolder.joinpath("png").exists():  # PNGs separated too
+        rasterfolder.joinpath("png").mkdir()
     aligncol, aligncond1, aligncond2, t_before, t_after, reg1, reg2 = alignsets[aligntime]
     fig, ax, sspkt, sspkclu, stdf = plot_twocond(
         eid,
@@ -182,6 +205,7 @@ for variable, (eid, pid, clu_id, region, drsq, aligntime) in targetunits.items()
         t_after,
         [reg1, reg2] if variable != "wheel" else ["wheel"],
     )
+    # Wheel only has one regressor, unlike all the others. Handle this properly
     if variable != "wheel":
         remstr = f"\n[{reg1}, {reg2}] regressors rem."
     else:
@@ -189,11 +213,9 @@ for variable, (eid, pid, clu_id, region, drsq, aligntime) in targetunits.items()
     names = [reg1, reg2, reg1 + remstr, reg2 + remstr]
     for subax, title in zip(ax[0, :], names):
         subax.set_title(title)
+    plt.savefig(varfolder.joinpath(f"{eid}_{pid}_clu{clu_id}_{region}_{variable}_predsummary.svg"))
     plt.savefig(
-        varfolder.joinpath(f"{eid}_{pid}_clu{clu_id}_{region}_{variable}_predsummary.svg")
-    )
-    plt.savefig(
-        varfolder.joinpath(f"{eid}_{pid}_clu{clu_id}_{region}_{variable}_predsummary.png")
+        varfolder.joinpath(f"png/{eid}_{pid}_clu{clu_id}_{region}_{variable}_predsummary.png")
     )
     plt.close()
 
@@ -211,14 +233,10 @@ for variable, (eid, pid, clu_id, region, drsq, aligntime) in targetunits.items()
         raster_cbar=True,
         raster_bin=RAST_BINSIZE,
     )
-    ax.set_title(
-        "{} unit {} : $\log \Delta R^2$ = {:.2f}".format(region, clu_id, np.log(drsq))
-    )
+    ax.set_title("{} unit {} : $\log \Delta R^2$ = {:.2f}".format(region, clu_id, np.log(drsq)))
+    plt.savefig(rasterfolder.joinpath(f"{eid}_{pid}_clu{clu_id}_{region}_{variable}_raster.svg"))
     plt.savefig(
-        rasterfolder.joinpath(f"{eid}_{pid}_clu{clu_id}_{region}_{variable}_raster.svg")
-    )
-    plt.savefig(
-        rasterfolder.joinpath(f"{eid}_{pid}_clu{clu_id}_{region}_{variable}_raster.png")
+        rasterfolder.joinpath(f"png/{eid}_{pid}_clu{clu_id}_{region}_{variable}_raster.png")
     )
     plt.close()
 
@@ -229,16 +247,7 @@ if not varfolder.exists():
     varfolder.mkdir()
 
 eid, pid, clu_id, region, drsq, aligntime = targetunits[variable]
-sessdf = bwm_query()
-subject = sessdf[sessdf["eid"] == eid]["subject"].iloc[0]
-eidfn = Path(GLM_CACHE).joinpath(Path(f"{subject}/{eid}/2022-12-22_{pid}_regressors.pkl"))
-stdf, sspkt, sspkclu, sclureg, clu_df = cw.get_cached_regressors(eidfn)
-design = cw.generate_design(stdf, stdf["probabilityLeft"], t_before=0.6, **glm_params)
-spkmask = sspkclu == clu_id
-nglm = lm.LinearGLM(
-    design, sspkt[spkmask], sspkclu[spkmask], estimator=glm_params["estimator"], mintrials=0
-)
-nglm.fit()
+stdf, sspkt, sspkclu, design, spkmask, nglm = load_unit_fit_model(eid, pid, clu_id)
 pred, trlabels = predict(nglm, glm_type="linear", retlab=True)
 mask = design.dm[:, design.covar["pLeft"]["dmcol_idx"]] != 0
 itipred = pred[clu_id][mask]
@@ -262,10 +271,6 @@ sns.boxplot(rates, x="pLeft", y="pred_rate", ax=ax[1])
 ax[0].set_title(f"{region} {clu_id} firing rate by block")
 ax[1].set_title(f"{region} {clu_id} predicted rate by block")
 ax[0].set_ylabel("Firing rate (spikes/s)")
-plt.savefig(
-    varfolder.joinpath(f"{eid}_{pid}_clu{clu_id}_{region}_{variable}_predsummary.svg")
-)
-plt.savefig(
-    varfolder.joinpath(f"{eid}_{pid}_clu{clu_id}_{region}_{variable}_predsummary.png")
-)
+plt.savefig(varfolder.joinpath(f"{eid}_{pid}_clu{clu_id}_{region}_{variable}_predsummary.svg"))
+plt.savefig(varfolder.joinpath(f"{eid}_{pid}_clu{clu_id}_{region}_{variable}_predsummary.png"))
 plt.close()
